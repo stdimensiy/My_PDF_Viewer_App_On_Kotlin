02257217e2328e9a1964e71b73de42bf45062667
d.veremeenko <stdimensiy@yandex.ru>
2021-10-27 13:54:50 +0300

    Изм 1.3 - реализация презентационной логики.  - Навешены кликлиссенеры  - реализована логика видимости невидимости (в базовом виде пока)  - созданы иконки  - улучшена разметка элемента списка

--- app/src/main/java/com/example/mypdfvieweronkotlin/ui/main/MainAdapter.kt
+++ app/src/main/java/com/example/mypdfvieweronkotlin/ui/main/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.mypdfvieweronkotlin.ui.main
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -25,6 +26,26 @@ class MainAdapter(_fragment: Fragment) : RecyclerView.Adapter<MainViewHolder>() 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         val item = items[position]
         holder.title.text = item.name
+        holder.iconDeleteOff()
+        holder.iconDownloadOn()
+        if (position == 2){
+            holder.iconDeleteOn()
+            holder.iconDownloadOff()
+        }
+    }
+
+    override fun onViewAttachedToWindow(holder: MainViewHolder) {
+        val item = items[holder.adapterPosition]
+        Log.d("Моя проверка", "Выполнился метод-сигнал , что вьюха приаттачена к окну")
+        holder.ivDownload.setOnClickListener {
+            Log.d("Моя проверка", "Отловлено событие нажания на кнопку загрузки $item")
+        }
+        holder.ivDelete.setOnClickListener {
+            Log.d("Моя проверка", "Отловлено событие нажания на кнопку Удаления $item")
+        }
+        holder.clItem.setOnClickListener {
+            Log.d("Моя проверка", "Отловлено событие нажатия на всю строку $item")
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/mypdfvieweronkotlin/ui/main/MainViewHolder.kt
+++ app/src/main/java/com/example/mypdfvieweronkotlin/ui/main/MainViewHolder.kt
@@ -1,10 +1,32 @@
 package com.example.mypdfvieweronkotlin.ui.main
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Visibility
 import com.example.mypdfvieweronkotlin.R
 
 class MainViewHolder(itemView: View):  RecyclerView.ViewHolder(itemView){
     val title: TextView = itemView.findViewById(R.id.title)
+    val ivDownload: ImageView = itemView.findViewById(R.id.imageView_download)
+    val ivDelete: ImageView = itemView.findViewById(R.id.imageView_delete)
+    val clItem: ConstraintLayout = itemView.findViewById((R.id.cl_item))
+
+    fun iconDeleteOff(){
+        ivDelete.visibility = View.GONE
+    }
+
+    fun iconDeleteOn(){
+        ivDelete.visibility = View.VISIBLE
+    }
+
+    fun iconDownloadOff(){
+        ivDownload.visibility = View.GONE
+    }
+
+    fun iconDownloadOn(){
+        ivDownload.visibility = View.VISIBLE
+    }
 }
