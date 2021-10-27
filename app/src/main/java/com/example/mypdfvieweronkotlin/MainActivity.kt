04518cc7723ada682502bb4db15ca7a437f08c93
Dmitriy <stdimensiy@yandex.ru>
2021-10-27 08:06:26 +0300

    Изм 1.1 Реализация загрузки, обновления и сохранения данных

--- app/src/main/java/com/example/mypdfvieweronkotlin/MainActivity.kt
+++ app/src/main/java/com/example/mypdfvieweronkotlin/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.mypdfvieweronkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.github.barteksc.pdfviewer.PDFView
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        downloadPdf("https://www.xeroxscanners.com/downloads/Manuals/XMS/PDF_Converter_Pro_Quick_Reference_Guide.RU.pdf")
+//
+//        var mContext: WeakReference<Context> = WeakReference(this)
+//        mContext.get()?.let {
+//
+//        }
+//        val a = filesDir
+//        val b = applicationContext.filesDir
+//        Log.d("Моя проверка", "Результат $a")
+//        Log.d("Моя проверка", "Результат $b")
+//
+//        var file = File(applicationContext.filesDir, "testfile2.txt").createNewFile()
+//        Log.d("Моя проверка", "Надеюсь получилось$file")
 
         val pdfview = findViewById<PDFView>(R.id.pdfview)
         pdfview.fromAsset("VDNH_test.pdf")
@@ -15,5 +31,34 @@ class MainActivity : AppCompatActivity() {
             //.enableSwipe( true) // включает свайп
             //.swipeHorizontal(true) // назначает горизонтальный свайп
             .load()
+    }
+
+    fun downloadPdf(pdfUrl: String) {
+        var request = Request.Builder().url(pdfUrl).build()
+        val client = OkHttpClient.Builder().build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("failed to download")
+                //complection(true)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                println("successful download")
+                val pdfData = response.body?.byteStream()
+                if (pdfData != null) {
+                    try {
+                        applicationContext.openFileOutput("myFile.pdf", Context.MODE_PRIVATE)
+                            .use { output ->
+                                output.write(pdfData.readBytes())
+                            }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+                //complection(true)
+            }
+
+        })
     }
 }
